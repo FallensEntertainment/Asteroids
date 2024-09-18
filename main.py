27ba1803a5b9379876f9e8c0a720b1766d85c0b0
FallensEntertainment <kacperskid6@gmail.com>
2024-09-18 11:44:22 +0200

    added player triangle and logic

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     print("Starting asteroids!",)
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,9 +21,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(0)
+        screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
